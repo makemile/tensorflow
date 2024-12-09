15137865f6831c690fb8572a442c57e8299f4fc0
makemile <mkengya@gmail.com>
2024-12-09 14:58:36 -0500

    knnclassifier

--- script2.js
+++ script2.js
@@ -1,44 +1,69 @@
+let net;
 let imgEl = document.getElementById("img");
 let descEl = document.getElementById("descripcion_imagen");
 const webCamElement = document.getElementById("webcam");
-let webcam = "";
+const classifier = knnClassifier.create();
 
-async function app() {
+const captureImg = async () => {
+  try {
+    const cam = await tf.data.webcam(webCamElement);
+    return await cam.capture();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const app = async () => {
   try {
     console.log("Loading Model...");
-    const net = await mobilenet.load();
+    net = await mobilenet.load();
     console.log(net, "Modelo cargado correctamente");
-
-    imgEl.onload = async function () {
-      const result = await net.classify(imgEl);
-      displayImagePrediction(result);
-
-      webcam = await tf.data.webcam(webCamElement);
-
-      while (true) {
-        const img = await webcam.capture();
-        const result = await net.classify(img);
-        document.getElementById("console").innerHTML =
-          "prediction:" +
-          result[0].className +
-          " probability:" +
-          result[0].probability;
-
-          img.dispose();
-          await tf.nextFrame();
-      }
-    };
 
     if (imgEl.complete) {
       const result = await net.classify(imgEl);
       displayImagePrediction(result);
     }
+
+    imgEl.onload = async () => {
+      const result = await net.classify(imgEl);
+      displayImagePrediction(result);
+    };
+
+    while (true) {
+      const img = await captureImg();
+      const result = await net.classify(img);
+      const activation = net.infer(img, "conv_preds");
+      let result2;
+
+      try {
+        result2 = await classifier.predictClass(activation);
+        const classes = ["Gatos", "Dino", "Elena", "Rock"];
+        document.getElementById("console2:" + classes[result2.label]);
+      } catch (error) {
+        console.error(error);
+      }
+      document.getElementById("console").innerHTML =
+        "prediction:" +
+        result[0].className +
+        " probability:" +
+        result[0].probability;
+
+      img.dispose();
+      await tf.nextFrame();
+    }
   } catch (err) {
     console.error("Error al cargar el modelo:", err);
   }
-}
+};
 
-async function displayImagePrediction(result) {
+const addExample = async (id) => {
+  const img = await captureImg();
+  const activation = net.infer(img, true);
+  classifier.addExample(activation, id);
+  img.dispose();
+};
+
+const displayImagePrediction = (result) => {
   if (result) {
     descEl.innerHTML = JSON.stringify(result, null, 2);
     console.log(result);
@@ -47,13 +72,13 @@ async function displayImagePrediction(result) {
       "El modelo no se cargó o el método classify no está disponible"
     );
   }
-}
+};
 
 let count = 0;
 
-async function cambiarImagen() {
+const cambiarImagen = async () => {
   count = count + 1;
   imgEl.src = "https://picsum.photos/200/300?random=" + count;
-}
+};
 
 app();
